c160a62d56cc4e9fe43812706bdbda37eb9527d0
moldir-baimu <moldir.baimu@gmail.com>
2023-02-05 20:27:31 +0000

    fixing code convention

--- file_test.py
+++ file_test.py
@@ -1,27 +1,32 @@
-import pytest
+# import pytest
+
 
 def test_calc_addition():
-  # function tests the output of 2+4
-  output = 2 + 4
-  assert output == 6
+        # function tests the output of 2+4
+    output = 2 + 4
+    assert output == 6
+
 
 def test_calc_substraction():
-  # function tests the output of 2-4
-  output  = 2 - 4
-  assert output == -2
+    # function tests the output of 2-4
+    output  = 2 - 4
+    assert output == -2
+
 
 def test_calc_multiply():
-  # function tests the output of 2*4
-  output = 2 * 4
-  assert output == 8
+    # function tests the output of 2*4
+    output = 2 * 4
+    assert output == 8
+
 
 def test_calc_division():
-  # function tests the output of 2/4
-  output = 2 / 4
-  assert output == 0.5
+    # function tests the output of 2/4
+    output = 2 / 4
+    assert output == 0.5
+
 
 def test_coucou():
-  # Function tests if the output return 'hello'
+    # Function tests if the output return 'hello'
     output='hello'
     assert output == 'hello'
 
